4ec0f43478298ff03440d2f02ff8ec2568aaa974
Oluwatofe <tsalako12@gmail.com>
2022-01-26 21:38:43 -0800

    project with imports
    just run `npm install` and then `npm run dev` and you should be on your way

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import 'bulma/css/bulma.min.css';
 
 export default function Home() {
   return (
